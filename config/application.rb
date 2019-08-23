7c4d719ed5157aa65768124a93047a67ae394def
alibabr <babar2166@gmial.com>
2019-08-23 15:26:02 +0500

    added precomile asserts

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
