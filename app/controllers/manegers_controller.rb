930124ceed24cf6e1bad756f73d61af77d03e628
alibabr <babar2166@gmial.com>
2019-08-30 00:56:36 +0500

    fixed styling and made manegers page

--- app/controllers/manegers_controller.rb
+++ app/controllers/manegers_controller.rb
@@ -3,15 +3,32 @@ class ManegersController < ApplicationController
 
   def index
     @user = current_user
+    @site = Site.all
   end
   def new
+    if params[:error].present?
+      @user = params[:error]
+    end
     @site = Site.find(params[:id])
   end
   def create
     if params[:password] == params[:confirm_password]
-      user = User.create(email: params[:email], password: params[:password], first_name: params[:first_name], last_name: params[:last_name])
-      Site.find(params[:site_id].to_i).users << user
+      @user = User.new(email: params[:email], password: params[:password], first_name: params[:first_name], last_name: params[:last_name])
+      if @user.save
+        Site.find(params[:site_id].to_i).users << @user
+        redirect_to :controller => 'sites', :action => 'index'
+      else
+        if params[:password] != params[:confirm_password]
+          @error = 'Password and confrim password should be same'
+        end
+        @site =  Site.find(params[:site_id])
+        render 'new'
+      end
+    else
+      @site =  Site.find(params[:site_id])
+      @error = 'Password and confrim password should be same'
+      render 'new'
     end
-    redirect_to :controller => 'sites', :action => 'index'
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_and_belongs_to_many :sites
+  validates_uniqueness_of :email
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
